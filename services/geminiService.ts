c41b21c06268158006a0d626d0929a0f86860332
Shrushti Zod <shrushtihemantzod@gmail.com>
2025-12-27 19:23:26 +0530

    Fixed pattenr parsing

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,7 +1,11 @@
 
 import { GoogleGenAI, Type } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+const ai = new GoogleGenAI({
+  apiKey: import.meta.env.VITE_GEMINI_API_KEY
+});
+
+
 
 export async function parsePatternWithAI(rawText: string) {
   const response = await ai.models.generateContent({
